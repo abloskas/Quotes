d2666c0338a362baed2ae80476c6727fb05ecac7
Ashley <a.bloskas@yahoo.com>
2018-05-08 15:18:35 -0700

    first commit

--- server.js
+++ server.js
@@ -6,20 +6,30 @@ var app = express();
 var bodyParser = require('body-parser');
 // Integrate body-parser with our App
 app.use(bodyParser.urlencoded({ extended: true }));
+const flash = require('express-flash');
+app.use(flash());
+var session = require('express-session');
+// more new code:
+app.use(session({
+  secret: 'keyboardkittehohhhhhhyyeaaaahhhhh',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { maxAge: 60000 }
+}))
 // Require path
 var path = require('path');
 // adding Mongoose
 var mongoose = require('mongoose');
-// This is how we connect to the mongodb database using mongoose -- "basic_mongoose" is the name of
-//   our db in mongodb -- this should match the name of the db you are going to use for your project.
-mongoose.connect('mongodb://localhost/basic_mongoose');
+
+mongoose.connect('mongodb://localhost/quote_dojo');
 // Mongoose schema
-var UserSchema = new mongoose.Schema({
-    name: String,
-    age: Number
-})
-mongoose.model('User', UserSchema); // We are setting this Schema in our Models as 'User'
-var User = mongoose.model('User') // We are retrieving this Schema from our Models, named 'User'
+var QuoteSchema = new mongoose.Schema({
+    name: { type: String, required: true, minlength: 6},
+    quote: { type: String, required: true, minlength: 10}
+},
+{timestamp:true});
+mongoose.model('Quote', QuoteSchema); 
+var Quote = mongoose.model('Quote');
 
 // Use native promises
 mongoose.Promise = global.Promise;
@@ -33,35 +43,43 @@ app.set('view engine', 'ejs');
 // Routes
 // Root Request
 app.get('/', function(req, res) {
-    // This is where we will retrieve the users from the database and include them in the view page we will be rendering.
-    User.find({}, function(err, users) {
-        // This is the method that finds all of the users from the database
-        // Notice how the first parameter is the options for what to find and the second is the
-        //   callback function that has an error (if any) and all of the users
-        // Keep in mind that everything you want to do AFTER you get the users from the database must
-        //   happen inside of this callback for it to be synchronous 
-        // Make sure you handle the case when there is an error, as well as the case when there is no error
-        res.render('index', {users: users});
-      }) 
+  res.render('index');
 })
-// Add User Request 
-app.post('/users', function(req, res) {
+app.get('/quotes', function(req, res){
+  Quote.find({}, function(err, quotes) {
+    if (err) {
+      console.log("We have an error!", err);
+      for(var key in err.errors){
+          req.flash('quoteErrors', err.errors[key].message);
+      }
+      res.redirect('/');
+    }
+   else {
+    res.render('quotes', {quotes: quotes});
+   }
+  }) 
+})
+
+app.post('/quotes', function(req, res) {
     console.log("POST DATA", req.body);
-    // create a new User with the name and age corresponding to those from req.body
-    var user = new User({name: req.body.name, age: req.body.age});
-    // Try to save that new user to the database (this is the method that actually inserts into the db) and run a callback function with an error (if any) from the operation.
-    user.save(function(err) {
+    var quote = new Quote({name: req.body.name, quote: req.body.quote});
+    quote.save(function(err) {
       // if there is an error console.log that something went wrong!
       console.log("hello");
       if(err) {
         console.log('something went wrong');
+        for(var key in err.errors){
+          req.flash('errors', err.errors[key].message);
+        }
         res.redirect("/");
       } else { // else console.log that we did well and then redirect to the root route
-        console.log('successfully added a user!');
-        res.redirect('/');
+        console.log('successfully added a quote!');
+        res.redirect('/quotes');
       }
     })
   })  
+
+  
 // Setting our Server to Listen on Port: 8000
 app.listen(8000, function() {
     console.log("listening on port 8000");
