843f6b319c262dd401e68ab446321ce99516b0f5
Ashley <a.bloskas@yahoo.com>
2018-05-10 13:09:20 -0700

    added MVC part2

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,42 +1,17 @@
 // Root Request
 
 const mongoose = require('mongoose'),
-      Quote = mongoose.model('Quote')
+Quote = mongoose.model('Quote')
+const quotes = require('../controllers/quotes')
 module.exports = function(app){
 app.get('/', function(req, res) {
-    res.render('index');
+    quotes.index(req, res);
   })
   app.get('/quotes', function(req, res){
-    Quote.find({}, function(err, quotes) {
-      if (err) {
-        console.log("We have an error!", err);
-        for(var key in err.errors){
-            req.flash('errors', err.errors[key].message);
-        }
-        res.redirect('/');
-      }
-     else {
-      res.render('quotes', {quotes: quotes});
-     }
-    }) 
+    quotes.quote(req, res);
   })
   
   app.post('/quotes', function(req, res) {
-      console.log("POST DATA", req.body);
-      var quote = new Quote({name: req.body.name, quote: req.body.quote});
-      quote.save(function(err) {
-        // if there is an error console.log that something went wrong!
-        console.log("hello");
-        if(err) {
-          console.log('something went wrong');
-          for(var key in err.errors){
-            req.flash('errors', err.errors[key].message);
-          }
-          res.redirect("/");
-        } else { // else console.log that we did well and then redirect to the root route
-          console.log('successfully added a quote!');
-          res.redirect('/quotes');
-        }
-      })
-    })  
+    quotes.postquote(req, res);
+})
 }
---
+++ server/controllers/quotes.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose'),
+Quote = mongoose.model('Quote')
+
+module.exports.index = function(req, res){
+    res.render('index');
+}
+
+module.exports.quote = function(req, res){
+    Quote.find({}, function(err, quotes) {
+        if (err) {
+          console.log("We have an error!", err);
+          for(var key in err.errors){
+              req.flash('errors', err.errors[key].message);
+          }
+          res.redirect('/');
+        }
+       else {
+        res.render('quotes', {quotes: quotes});
+       }
+      }) 
+}
+
+module.exports.postquote = function(req, res){
+    console.log("POST DATA", req.body);
+      var quote = new Quote({name: req.body.name, quote: req.body.quote});
+      quote.save(function(err) {
+        // if there is an error console.log that something went wrong!
+        console.log("hello");
+        if(err) {
+          console.log('something went wrong');
+          for(var key in err.errors){
+            req.flash('errors', err.errors[key].message);
+          }
+          res.redirect("/");
+        } else { // else console.log that we did well and then redirect to the root route
+          console.log('successfully added a quote!');
+          res.redirect('/quotes');
+        }
+      })
+    }
